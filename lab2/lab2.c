574c34d403a8c6ee89befa43433f9db62c20eeb2
Shiv <shivgunter@gmail.com>
2019-10-16 14:17:42 +0100

    done

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 #include "revwords.h"
+
+static char answers[][80] = {
+  "ytisrevinU",
+  "ytisrevinU fo egdirbmaC",
+  "olleH, dlrow",
+  " olleH",
+  " ytisrevinU fo egdirbmaC!",
+  "ytisrevinU fo egdirbmaC",
+  "-$$$!!!-",
+  "ytisrevinU-fo-egdirbmaC"
+};
 
 static char strings[][80] = {
   "University",
   "University of Cambridge",
   "Hello, world",
-  " Hello ",
-  "University of Cambridge!",
-  " University of Cambridge",
+  " Hello",
+  " University of Cambridge!",
+  "University of Cambridge",
   "-$$$!!!-",
   "University-of-Cambridge"
 };
 
-int main(void) { 
+int main(void) {
   for (int i = 0; i < 8; i++) {
     printf("Original: %s\n", strings[i]);
     reverse_words(strings[i]);
     printf("Reversed: %s\n", strings[i]);
+    printf("%d\n", strcmp(strings[i], answers[i]));
   }
   return 0;
 }
@@ -29,8 +42,8 @@ Original: University of Cambridge
 Reversed: ytisrevinU fo egdirbmaC
 Original: Hello, world
 Reversed: olleH, dlrow
-Original:  Hello 
-Reversed:  olleH 
+Original:  Hello
+Reversed:  olleH
 Original: University of Cambridge!
 Reversed: ytisrevinU fo egdirbmaC!
 Original:  University of Cambridge
@@ -43,5 +56,5 @@ Reversed: ytisrevinU-fo-egdirbmaC
 */
 
 
-  
-  
+
+
--- lab2/revwords.c
+++ lab2/revwords.c
@@ -1,22 +1,87 @@
 #include <ctype.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
 #include "revwords.h"
 
-void reverse_substring(char str[], int start, int end) { 
+void reverse_substring(char str[], int start, int end) {
   /* TODO */
+  int len = (end-start+1)/2; // truncation towards zero
+  for (int i = 0; i < len; i++)
+  {
+      char tmp = str[start];
+      str[start++] = str[end];
+      str[end--] = tmp;
+  }
 }
 
 
-int find_next_start(char str[], int len, int i) { 
+int find_next_start(char str[], int len, int i) {
   /* TODO */
-  return 0;
-}
+  // assume i >= 0
+  // check if there is a word starting at i
+  if (i >= len) {
+      return -1;
+  }
+  // keep moving forward until you find a letter
+  bool spacePrev = (i == 0) ? true : false;
+    while (i < len) {
+        // if current index is a character
+        if (isalpha(str[i])) {
+            if (spacePrev) {
+                break;
+            }
+            else {
+                spacePrev = false;
+                i++;
+            }
+        }
+        else {
+            spacePrev = true;
+            i++;
+        }
+    }
+    // now i is len or i is start of word
+    if (i == len) {
+        return -1;
+    }
+    else {
+        return i;
+    }
+  }
 
 int find_next_end(char str[], int len, int i) {
   /* TODO */
-  return 0;
+  if (i >= len) {
+      return -1;
+  }
+    while (i < len) {
+        // if current index is not a character
+        if (!isalpha(str[i])) {
+            break;
+        }
+        else {
+            i++;
+        }
+    }
+    // now i is len or i is start of word
+    return i;
 }
 
-void reverse_words(char s[]) { 
+void reverse_words(char s[]) {
   /* TODO */
+  // for word in s
+    // reverse the word in-place
+    int len = strlen(s);
+    int i = 0;
+    int start, end;
+    while (i < len) {
+        start = find_next_start(s, len, i);
+        if (start == -1) {
+            return;
+        }
+        end = find_next_end(s, len, start);
+        reverse_substring(s, start, end-1);
+        i = end;
+    }
 }

b6e4c5e404d8ef3c68404737b4e9b874bd42b266
Shiv <shivgunter@gmail.com>
2020-01-10 21:40:06 +0000

    done

--- lab8/matrix.c
+++ lab8/matrix.c
@@ -3,49 +3,94 @@
 
 matrix_t matrix_create(int rows, int cols) { 
   /* TODO */
-  matrix_t m = {0, 0, NULL};
+  double * elts = malloc(rows*cols*sizeof(double));
+  matrix_t m = {rows, cols, elts};
   return m;
 }
 
 double matrix_get(matrix_t m, int r, int c) { 
   /* TODO */
   assert(r < m.rows && c < m.cols);
-  return 0.0;
+  return m.elts[r*m.cols+c];
 }
 
 void matrix_set(matrix_t m, int r, int c, double d) { 
   assert(r < m.rows && c < m.cols);
   /* TODO */
+  m.elts[r * m.cols + c] = d;
 }
 
 
 void matrix_free(matrix_t m) { 
   /* TODO */
+  free(m.elts);
 }
 
-matrix_t matrix_multiply(matrix_t m1, matrix_t m2) { 
-  /* TODO */
-  matrix_t m = {0, 0, NULL};
-  return m;
+matrix_t matrix_multiply(matrix_t m1, matrix_t m2)
+{
+    assert(m1.cols == m2.rows);
+
+    matrix_t m = matrix_create(m1.rows, m2.cols);
+
+    for (int i = 0; i < m1.rows; i++)
+    {
+        for (int j = 0; j < m2.cols; j++)
+        {
+            double sum = 0.0;
+            for (int l = 0; l < m1.cols; l++)
+            {
+                sum += matrix_get(m1, i, l) * matrix_get(m2, l, j);
+            }
+            matrix_set(m, i, j, sum);
+        }
+    }
+
+    return m;
 }
 
-matrix_t matrix_transpose(matrix_t m) { 
-  /* TODO */
-  matrix_t t = {0, 0, NULL}; 
-  return t;
+matrix_t matrix_transpose(matrix_t m)
+{
+    matrix_t t = matrix_create(m.cols, m.rows);
+
+    for (int i = 0; i < m.rows; i++)
+    {
+        for (int j = 0; j < m.cols; j++)
+        {
+            double a = matrix_get(m, i, j);
+            matrix_set(t, j, i, a);
+        }
+    }
+    return t;
 }
 
-matrix_t matrix_multiply_transposed(matrix_t m1, matrix_t m2) { 
-  assert(m1.cols == m2.cols);
-  /* TODO */
-  matrix_t m = {0, 0, NULL};
-  return m;
+matrix_t matrix_multiply_transposed(matrix_t m1, matrix_t m2)
+{
+    assert(m1.cols == m2.cols);
+
+    matrix_t m = matrix_create(m1.rows, m2.rows);
+
+    for (int i = 0; i < m1.rows; i++)
+    {
+        for (int j = 0; j < m2.rows; j++)
+        {
+            double sum = 0.0;
+            for (int l = 0; l < m1.cols; l++)
+            {
+                sum += matrix_get(m1, i, l) * matrix_get(m2, j, l);
+            }
+            matrix_set(m, i, j, sum);
+        }
+    }
+
+    return m;
 }
 
-matrix_t matrix_multiply_fast(matrix_t m1, matrix_t m2) { 
-  /* TODO */
-  matrix_t result = {0, 0, NULL};
-  return result;
+matrix_t matrix_multiply_fast(matrix_t m1, matrix_t m2)
+{
+    matrix_t trans = matrix_transpose(m2);
+    matrix_t result = matrix_multiply_transposed(m1, trans);
+    matrix_free(trans);
+    return result;
 }
 
 void matrix_print(matrix_t m) { 
